3cc24e7eb7c2e70a523584d3e44304422367ad06
pengrui <pengrui55555@163.com>
2019-03-10 13:07:30 +0800

    更新 多进程问题未解决

--- crawler/use_case/douban_multiThread_spiders.py
+++ crawler/use_case/douban_multiThread_spiders.py
@@ -49,7 +49,7 @@ def 多线程爬取示例():
         comments = body.xpath('//*[@id="comments"]/div/div[2]/p/span/text()')
         writter.write_buffer([comments])
 
-    main函数实现
+    main函数实现, level表示可深入URL的层级，以下是通过网页深度抓取，所以可以for i in range(1,100,20)来模拟6个初始网页进行抓取，也可以只提供第一个URL，然后设置level为6进行深度抓取
 
     >>>
     if __name__ == '__main__':
@@ -59,6 +59,6 @@ def 多线程爬取示例():
                 'url': 'https://movie.douban.com/subject/26810318/comments?start={}&limit=20&sort=new_score&status=P'.format(i)
             })
         spider = Spider(requests,
-                        getUrl_func=geturl, getData_func=getdata, level=3)
+                        getUrl_func=geturl, getData_func=getdata, level=1)
         spider.start_multiThread_crawl(4)
     '''
