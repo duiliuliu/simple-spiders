8e617acc6917c699db1d2d122c1e721c84289204
pengrui <pengrui55555@163.com>
2019-03-13 09:47:24 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import sys
  
 setup(
     name="simple-spiders",
-    version="0.1.4",
+    version="0.1.5",
     author="pengr",
     author_email="pengrui55555@163.com",
     description="A simple web crawling framework.",
